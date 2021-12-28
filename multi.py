b8b0a559dd5b5dd97732dc6aca96675f642d2165
THROUGH-REALITY <89965458+THROUGH-REALITY@users.noreply.github.com>
2021-12-28 12:20:34 +0900

    12/28 12:20

--- multi.py
+++ multi.py
@@ -9,9 +9,9 @@ from qlearning_agent import Summon
 from main import Plotting
 
 # 定数
-NB_EPISODE = 50   # エピソード数
-X_MAX = 10
-Y_MAX = 10
+NB_EPISODE = 31   # エピソード数
+X_MAX = 12
+Y_MAX = 12
 POPULATION = 10
 start = time.time()
 
@@ -20,8 +20,19 @@ if __name__ == '__main__':
         x_max=X_MAX,
         y_max=Y_MAX)
 
-    summon = Summon(    # エージェントの召喚
-        zero_list=grid_env.zero_list,
+    decided_zero = [grid_env.zero_list[10],
+                    grid_env.zero_list[16],
+                    grid_env.zero_list[23],
+                    grid_env.zero_list[39],
+                    grid_env.zero_list[44],
+                    grid_env.zero_list[51],
+                    grid_env.zero_list[53],
+                    grid_env.zero_list[68],
+                    grid_env.zero_list[79],
+                    grid_env.zero_list[91]]
+    
+    summon = Summon(             # エージェントの召喚
+        zero_list=decided_zero,
         population=POPULATION)
     print(f"StaRt time = {time.time()-start}")    #times = []
 
@@ -52,29 +63,33 @@ if __name__ == '__main__':
             #print(summon.agents[id].observation)
         #agent.observe(state)    # エージェントを初期位置に
         print(f"EP.{episode +1} End time = {time.time()-start}") #(t = {len(episode_reward)})"# 所要時間の計算
-    fig = plt.figure(tight_layout=True)  # 図を描く大きさと、図の変数名を宣言
+    fig = plt.figure(figsize=(18,10), tight_layout=True)  # 図を描く大きさと、図の変数名を宣言
     gs = fig.add_gridspec(1, 3)
     ax1 = fig.add_subplot(
         gs[0, 0],
         xlabel="X",
         ylabel="Y",
         title="init_state")
-    ax1.text(0, 0.2, 'G', ha='center')
+    ax1.text(0, 0, 'G', size=15, ha='center', va='center', color='white')
     ax1.imshow(init_map)
     ax2 = fig.add_subplot(
         gs[0, 1:],
         xlabel="episode",
-        ylabel="Agents_ID",
+        ylabel="times",
         title="Result",
-        xlim=(0,NB_EPISODE))
-    for id in range(POPULATION):
-        ax2.plot(np.arange(NB_EPISODE),episode_reward[id])
+        xlim=(0,NB_EPISODE-1))
+    lines = [ax2.plot(np.arange(NB_EPISODE),episode_reward[id]) for id in range(POPULATION)]
+    y_min, max_y = ax2.get_ylim()
+    ax2.set_ylim(0, max_y)
+    
+    #ax2.legend(handles=lines[::-1], labels=[e for e in range(POPULATION)])
+    fig.legend()
     ax1.xaxis.set_major_locator(ticker.MultipleLocator())
     ax1.yaxis.set_major_locator(ticker.MultipleLocator())
     ax2.xaxis.set_major_locator(ticker.MultipleLocator(5))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator(100))
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(100)) 
     #Plotting.text(episode_reward,ax2,"deeppink")
     for index,value in enumerate(summon.agents):
-        ax1.text(value.init_pos[1], value.init_pos[0], index, size=12, color='deeppink', ha='center', va='center')
+        ax1.text(value.init_pos[1], value.init_pos[0], index, size=15, color='deeppink', ha='center', va='center')
     plt.savefig(f"result.png")
     plt.show()
