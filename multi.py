7efa8b577e565eb029a9fba6490053cd3cd4cf48
THROUGH-REALITY <through.reality201512@gmail.com>
2022-01-05 14:37:44 +0900

    01/05 14:37

--- multi.py
+++ multi.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
 
     #print(f"StaRt time = {time.time()-start}")    #times = []
 
-    action_exp = [0,2]
+    action_exp = [2,3]
 
     episode_reward = np.zeros((NB_EPISODE, POPULATION))  # 1エピソードの累積報酬
     for episode in range(NB_EPISODE):   # 実験
@@ -51,15 +51,18 @@ if __name__ == '__main__':
             grid_env.map[start_x][start_y] = 3
         init_map = np.array(grid_env.map)
         while(np.all(is_end_episode) == False):    # 全員がゴールするまで続ける
-            #plt.imshow(grid_env.map)
-            #plt.show()
+            plt.imshow(grid_env.map)
+            plt.show()
             for id,agent in enumerate(summon.agents):
                 if is_end_episode[id] == False:
                     start_x = agent.observation[0]
                     start_y = agent.observation[1]
                     grid_env.map[start_x][start_y] = 0
-                    action = action_exp[int(episode_reward[episode][id])%2]
+                    action = 0
                     state, is_end_episode[id] = grid_env.step(start_x, start_y, action)
+                    if state == (start_x,start_y):
+                        action = random.choice(action_exp)
+                        state, is_end_episode[id] = grid_env.step(start_x, start_y, action)
                     grid_env.map[state[0]][state[1]] = 3
                     agent.observe(state)   # 状態と報酬の観測 
                     episode_reward[episode][id] += 1
@@ -97,7 +100,7 @@ if __name__ == '__main__':
     ax1.xaxis.set_major_locator(ticker.MultipleLocator())
     ax1.yaxis.set_major_locator(ticker.MultipleLocator())
     ax2.xaxis.set_major_locator(ticker.MultipleLocator(5))
-    ax2.yaxis.set_major_locator(ticker.MultipleLocator()) 
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(10)) 
     #Plotting.text(episode_reward,ax2,"deeppink")
     for index,value in enumerate(summon.agents):
         ax1.text(value.init_pos[1], value.init_pos[0], index, size=15, color='deeppink', ha='center', va='center')
